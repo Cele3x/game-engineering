2b69a42a23f58be23e0f9e069f9cae52481e9861
Alexander Kalus <Alexander.Kalus@stud.uni-regensburg.de>
2020-04-07 20:09:45 +0200

    replaced spray can model and added animation when spraying

--- src/Assets/Scripts/SprayLauncher.cs
+++ src/Assets/Scripts/SprayLauncher.cs
@@ -6,6 +6,10 @@ public class SprayLauncher : MonoBehaviour
 
 {
     public ParticleSystem particleLauncher;
+    public GameObject nozle;
+    public GameObject hole;
+    private Vector3 nozlePosSteady = new Vector3(0, 0, 0);
+    private Vector3 nozlePosPressed = new Vector3(0, (float)-0.1, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -13,11 +17,23 @@ public class SprayLauncher : MonoBehaviour
         
     }
 
-  
+
     // Update is called once per frame
     void Update()
     {
-    if (Input.GetButton("Fire1")) { /*particleLauncher.Play();*/particleLauncher.Emit(5); }
-        
+        if (Input.GetButton("Fire1"))
+        { /*particleLauncher.Play();*/
+            particleLauncher.Emit(5);
+
+            nozle.transform.localPosition = nozlePosPressed;
+            hole.transform.localPosition = nozlePosPressed;
+
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            nozle.transform.localPosition = nozlePosSteady;
+            hole.transform.localPosition = nozlePosSteady;
+        }
     }
 }
