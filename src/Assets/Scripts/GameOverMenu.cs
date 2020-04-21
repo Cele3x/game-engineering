ac9a6adf983ad02db67e17a9b140538e32fb0cbf
KevinA1224 <KevinA1224@users.noreply.github.com>
2020-04-21 05:01:17 +0200

    game over screen displays final time
    The game over screen now shows how long the player survived against the wasps.

--- src/Assets/Scripts/GameOverMenu.cs
+++ src/Assets/Scripts/GameOverMenu.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+using TMPro;
 
 public class GameOverMenu : MonoBehaviour
 {
+
+    public static bool gameIsOver = false;
+    public GameObject gameOverMenuUI;
+
+    private PlayerController playerController;
+    [SerializeField] private TextMeshProUGUI displayTimerText;
+    [SerializeField] private TextMeshProUGUI ingameTimerText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(playerController.Health <= 0)
+        {
+            gameIsOver = true;
+        }
+
+        if (gameIsOver)
+        {
+            displayTimerText.text = ingameTimerText.text;
+            Time.timeScale = 0f;
+            AudioListener.pause = true;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
+
+    public void ReturnToTitle()
+    {
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+        SceneManager.LoadScene(0);
+        gameOverMenuUI.SetActive(false);
+        gameIsOver = false;
+    }
+
 }
