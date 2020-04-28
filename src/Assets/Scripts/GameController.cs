afd0a032847e9d8c654f4fa88c8148d3a52bde35
KevinA1224 <KevinA1224@users.noreply.github.com>
2020-04-28 04:46:02 +0200

    changed powerup spawn behavior
    The powerups now spawn less frequently to make them more special. Also only one powerup is now active the entire time

--- src/Assets/Scripts/GameController.cs
+++ src/Assets/Scripts/GameController.cs
@@ -10,9 +10,15 @@ public class GameController : MonoBehaviour
     public GameObject playerBody;
     public GameObject beePrefab;
     public GameObject powerUpPrefab;
+
     private GameObject parent;
     private PlayerController playerController;
     private int beeCounter = 0;
+
+    //sets the interval of time in which the powerup spawns
+    private float powerUpRandomSpanMin = 30;
+    private float powerUpRandomSpanMax = 35;
+
     public int playerScore = 0;
     public int beeScore = 0;
     
@@ -20,7 +26,9 @@ public class GameController : MonoBehaviour
                                                        new Vector3(10, 0, -20), new Vector3(-30, 0, -20)};
 
     private readonly Vector3[] powerupSpawnPoints = new[] { new Vector3(-4, 1, -4), new Vector3(2, 1, -30),};
+
     private bool powerUpRoutineActive = false;
+    private bool powerUpInstanceActive = false;
 
     private int _currentSpawnIndex = 0;
     
@@ -33,8 +41,8 @@ public class GameController : MonoBehaviour
     }
     void Update()
     {
-
-        if (powerUpRoutineActive == false)
+        //checks if powerup routine is active and if a powerup object already exists, if it doesn't, then activate the routine to spawn a new one
+        if (!powerUpRoutineActive && !powerUpInstanceActive)
         {
             powerUpRoutineActive = true;
             StartCoroutine(SpawnPowerup());
@@ -48,6 +56,7 @@ public class GameController : MonoBehaviour
         bee.GetComponent<BeeController>().target = playerBody.transform;
         bee.name = "Bee_" + beeCounter++;
     }
+
     private void InstantiatePowerUp(GameObject powerUp)
     {
 
@@ -78,18 +87,18 @@ public class GameController : MonoBehaviour
     public void CollectSpray()
     {
         playerController.CollectSpray();
+        powerUpInstanceActive = false;
 
     }
 
     IEnumerator SpawnPowerup()
     {
-        yield return new WaitForSeconds(Random.Range(10,15));
+        yield return new WaitForSeconds(Random.Range(powerUpRandomSpanMin, powerUpRandomSpanMax));
 
-       
-        //if not game over:
-        
+        //create a new spray powerup that can be collected by the user        
         InstantiatePowerUp(powerUpPrefab);
-        
+
+        powerUpInstanceActive = true;
         powerUpRoutineActive = false;
     }
 
