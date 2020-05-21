e1cb0ee729b4bedd2e69b131577d93a60a9d872e
Alexander Kalus <Alexander.Kalus@stud.uni-regensburg.de>
2020-05-21 22:23:39 +0200

    Some Spray Powerup reworks
    - set Can to not static, since this caused a bug
    - removed animator and implemented hover animation in code as part of powerupbehaviour
    - removed rigidbody from spray

--- src/Assets/Scripts/PowerupBehaviour.cs
+++ src/Assets/Scripts/PowerupBehaviour.cs
@@ -6,13 +6,32 @@ public class PowerupBehaviour : MonoBehaviour
 {
     private GameController gameController;
     private bool triggered = false;
+    private float speed = 0.5f;
+    private float up = 1;
+    private float down = 0.5F;
+
+
+    private Vector3 direction = Vector3.up;
 
     void Start()
     {
-
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
     }
+ 
 
+    void Update()
+    {
+        gameObject.transform.Translate(direction * Time.smoothDeltaTime*speed);
+
+        if (gameObject.transform.position.y > up)
+        {
+            direction = Vector3.down;
+        }
+        else if (gameObject.transform.position.y < down)
+        {
+            direction = Vector3.up;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("ontriggerenter");
@@ -29,21 +48,5 @@ public class PowerupBehaviour : MonoBehaviour
             
         }
     }
-    /* private void OnCollisionEnter(Collision collision)
- {
-     Debug.Log("collision power up");
-     if (triggered == false)
-     {
-         Debug.Log("" + collision.other);
-         if (collision.other.CompareTag("Player"))
-         {
-             triggered = true;
-             //audioSource.Play(0);
-             //rend.enabled = false;
 
-             gameController.CollectSpray();
-         }
-
-     }
- }*/
 }
