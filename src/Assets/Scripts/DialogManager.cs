2d5d7d77c20573d607f5cba1c044538679df4f3b
Alexander Kalus <Alexander.Kalus@stud.uni-regensburg.de>
2020-05-23 16:35:14 +0200

    Made Spray Explanation more precise

--- src/Assets/Scripts/DialogManager.cs
+++ src/Assets/Scripts/DialogManager.cs
@@ -22,7 +22,7 @@ public class DialogManager : MonoBehaviour {
         "Dies ist eine Wespe. Hüten Sie sich vor ihr, sie ist aggressiv und will Sie stechen! Schlagen Sie besser mit Ihrer Klatsche zu, bevor sie Sie tötet!",
          "Diese Anzeige zählt alle Wespen, die Sie im  aktuellen Durchgang eliminiert haben. Können Sie den Highscore toppen?",
                  "Das hier zeigt Ihren aktuellen Gesundheitszustand an. Wenn Sie von einer Biene gestochen werden, verlieren Sie ein Leben. Wenn Sie keine Leben mehr haben, verlieren Sie das Spiel. So einfach ist das.",
-        "Wenn Sie Glück haben, finden Sie ein Insektenspray wie dieses hier. Laufen Sie durch, um es aufzunehmen, und versprühen Sie mit der rechten Maustaste giftiges Insektenspray. Wespen hassen diesen Trick, es macht sie ganz benommen.",
+        "Wenn Sie Glück haben, finden Sie ein Spray wie dieses hier. Laufen Sie durch, um es aufzunehmen, und versprühen Sie mit der rechten Maustaste giftiges Insektenspray. Wespen hassen diesen Trick, es macht sie ganz langsam und träge.",
         "Schon Oma wusste, dass Zwiebeln gegen Wespenstiche helfen. Sammeln Sie Zwiebeln und erhalten Sie ein Leben zurück. Wie bei den Sprühdosen muss man sie im ganzen Areal gut suchen.",
          "Das war's fürs Erste, legen Sie gleich los, indem Sie auf den Start-Button unten rechts klicken, sobald Sie bereit sind."
     };
@@ -31,7 +31,7 @@ public class DialogManager : MonoBehaviour {
         "This is a wasp. Beware of it, it is aggressive and wants to sting you! Better hit it with your swatter before it kills you!",
         "This Display keeps count of all the wasps you have defeated during the current run. Can you beat the highscore?",
         "This is your healthbar. It shows your current health. You lose one live if a bee stings you. If you dont have any lives left you loose the game. It's as simple as that.",
-    "Sometimes you'll be lucky and find an insect spray like this one here. Run through it to pick it up and use your right mouse button to emit toxic insect spray. Wasps hate that trick, it makes them dizzy.",
+    "Sometimes you'll be lucky and find an insect spray like this one here. Run through it to pick it up and use your right mouse button to emit toxic insect spray. Wasps hate that trick, it makes them so slow.",
     "Grandma already knew that onions help fight wasp stings. Gather onions and gain extra health. Like the spray cans, you have to look for them carefully in the whole area",
     "Thats all for now, dive right into it by clicking the start button in the bottom right, when you are ready!",};
 
